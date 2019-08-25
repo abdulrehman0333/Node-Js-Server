d614b7c8ac0fc0a34e6ca5d7bf5bc9945a43360d
SYED ABDUL-REHMAN ALI SHAH <44140937+abdulrehman0333@users.noreply.github.com>
2019-08-25 11:26:57 +0500

    Add files via upload
    Initial Change

--- Node Js/server.js
+++ Node Js/server.js
@@ -2,10 +2,16 @@ console.log("Hello World!")
 const express = require("express");
 const app = express();
 
+app.use(express.json())
+
 app.listen(3002,()=>{
     console.log("Server is running on port 3002");
 });
 
-app.get("/",(req , res)=>{
-    res.send({message: "Hello World!"})
+// app.get("/getdata",(req , res)=>{
+//     res.send({message: "Hello World! from GET"})
+// })
+
+app.post("/getdata",(req , res)=>{
+    console.log("Hello World! from POST", req.body)
 })
